85c82f7ddf36b9d7b2057eb75865115605325b3a
Commandcracker <4083674-Commandcracker@users.noreply.gitlab.com>
2021-07-30 13:01:37 +0200

    fixed loadAPIFromURL

--- src/installer.lua
+++ src/installer.lua
@@ -51,40 +51,12 @@ local function get(url)
     end
 end
 
-local tAPIsLoading = {}
 local function loadAPIFromURL(url, name)
-    if tAPIsLoading[name] == true then
-        printError( "API "..name.." is already being loaded" )
-        return false
-    end
-    tAPIsLoading[name] = true
-
-    local tEnv = {}
-    setmetatable( tEnv, { __index = _G } )
-    local fnAPI, err = load( get(url), tEnv )
-    if fnAPI then
-        local ok, err = pcall( fnAPI )
-        if not ok then
-            printError( err )
-            tAPIsLoading[name] = nil
-            return false
-        end
-    else
-        printError( err )
-        tAPIsLoading[name] = nil
-        return false
-    end
-    
-    local tAPI = {}
-    for k,v in pairs( tEnv ) do
-        if k ~= "_ENV" then
-            tAPI[k] =  v
-        end
-    end
-
-    _G[name] = tAPI    
-    tAPIsLoading[name] = nil
-    return true
+    api_path = "/tmp/"..name
+    apt_file = fs.open(api_path,"w")
+    apt_file.write(get(url))
+    os.loadAPI(api_path)
+    fs.delete(api_path)
 end
 
 -- cli
