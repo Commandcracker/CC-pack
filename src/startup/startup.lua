2a13f4f2fa8c85d183b8c288fbeaa54a6cb01182
Commandcracker <4083674-Commandcracker@users.noreply.gitlab.com>
2021-07-31 15:28:09 +0200

    fixed Completion

--- src/startup/startup.lua
+++ src/startup/startup.lua
@@ -1,7 +1,24 @@
+local function completeMultipleChoice( sText, tOptions, bAddSpaces )
+    local tResults = {}
+    for n=1,#tOptions do
+        local sOption = tOptions[n]
+        if #sOption + (bAddSpaces and 1 or 0) > #sText and string.sub( sOption, 1, #sText ) == sText then
+            local sResult = string.sub( sOption, #sText + 1 )
+            if bAddSpaces then
+                table.insert( tResults, sResult .. " " )
+            else
+                table.insert( tResults, sResult )
+            end
+        end
+    end
+    return tResults
+end
+
 shell.setCompletionFunction(
     fs.getDir(shell.getRunningProgram()).."/bin/pack", 
     function(shell, index, text)
-        if index == 2 then return end
-        return {"install","show","search","remove","list","fetch","add-source"}
+        if index == 1 then
+            return completeMultipleChoice(text, {"install","show","search","remove","list","fetch","add-source"})
+        end
     end
 )
